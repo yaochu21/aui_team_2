6b7ae2d2075479bf09ba619bb710bb64a2f30aab
Spencer Ng <sng1488@gmail.com>
2023-02-13 01:15:54 -0600

    Add sample python server code

--- python_server/main.py
+++ python_server/main.py
@@ -1,8 +1,55 @@
 from pythonosc import udp_client
 from pythonosc.dispatcher import Dispatcher
+from time import sleep
 
-client = udp_client.SimpleUDPClient("127.0.0.1", 5005)
+from typing import List, Any
 
-def snap_position(*args: List[float])
+from threading import Thread
 
-client.send_message("/toio/hr_cursor/pos", (x_pos, y_pos))
+FRAME_RATE = 30
+
+class ToioManager:
+    def __init__(self):
+        self.client = udp_client.SimpleUDPClient("127.0.0.1", 3334)
+        self.disp = Dispatcher()
+        self.disp.map("/toio_input/*", self.handle_toio_pos_update)
+
+        while True:
+            self.update_position()
+            sleep(1 / FRAME_RATE)
+
+    def handle_toio_pos_update(self, address: str, x_pos: float, y_pos: float):
+        if "/hr_pos" in address:
+            self.hr_x = x_pos
+            self.hr_y = y_pos
+        elif "/timeline_pos" in address:
+            self.timeline_x = x_pos
+            self.timeline_y = y_pos
+
+    def update_projections(self):
+        # TODO: update this with calculated parameters(?) for projections stuff
+        pass
+
+    def find_closest_point(self, x, y):
+        # Sample function for updating the closest point on a grid
+        # TODO: use the actual data for this to snap a toio
+        return x + 10, y + 10
+
+    def update_position(self):
+        # TODO: update this with calculated x and y values for each toio
+        x_pos = 0
+        y_pos = 0
+
+        # Example code below for accessing class vars
+        hr_pos = self.find_closest_point(self.hr_x, self.hr_y)
+
+        self.client.send_message("/toio/hr", hr_pos)
+        self.client.send_message("/toio/timeline", (x_pos, y_pos))
+        self.client.send_message("/toio/star/top_l", (x_pos, y_pos))
+        self.client.send_message("/toio/star/top_r", (x_pos, y_pos))
+        self.client.send_message("/toio/star/bot_l", (x_pos, y_pos))
+        self.client.send_message("/toio/star/bot_r", (x_pos, y_pos))
+        self.client.send_message("/toio/planet_orbit", (x_pos, y_pos))
+
+if __name__ == "__main__":
+    ToioManager()
